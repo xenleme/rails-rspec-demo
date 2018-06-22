7fc26915cb9f838e0372c41e07d1ad81bada0ca3
xenleme <xenleme@gmail.com>
2018-06-22 20:59:15 +0300

    Update articles controller & views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.fetch(:article, {})
+      params.require(:article).permit(:active, :id, :title, :body)
     end
 end
